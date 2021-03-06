8625cff6e1383b11dbd5aef76e17fe7583ef1b89
maemae9312 <hc081110@gmail.com>
2021-03-06 16:43:24 +0900

    add class name

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ export const App = () => {
   return (
     <>
     {/* 完了エリア */}
-      <div>
+      <div className='input-area' >
         <input placeholder='Enter TODO' />
         <button>Add</button>
       </div>
 
       {/* 未完了エリア */}
-      <div>
-        <p>Unfinished TODO</p>
+      <div className='incomplete-area' >
+        <p className='title' >Unfinished TODO</p>
         <ul>
-          <div>
+          <div className='list-row' >
             <li>todo 1</li>
             <button>Done</button>
             <button>Delete</button>
           </div>
-          <div>
+          <div className='list-row' >
             <li>todo 2</li>
             <button>Done</button>
             <button>Delete</button>
@@ -28,20 +28,14 @@ export const App = () => {
       </div>
 
       {/* 完了エリア */}
-      <div>
-      <div>
-        <p>Completed TODO</p>
+      <div className='complete-area'>
+        <p className='title' >Completed TODO</p>
         <ul>
-          <div>
+          <div className='list-row' >
             <li>todo 3</li>
             <button>Return</button>
           </div>
-          <div>
-            <li>todo 4</li>
-            <button>Return</button>
-          </div>
         </ul>
-      </div>
       </div>
     </>
   )

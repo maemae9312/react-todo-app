b70943649f28d073ac60fcdadab4255ab564ce15
maemae9312 <hc081110@gmail.com>
2021-03-06 16:55:58 +0900

    implementation of map function

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,15 @@ export const App = () => {
       <div className='incomplete-area' >
         <p className='title' >Unfinished TODO</p>
         <ul>
-          <div className='list-row' >
-            <li>todo 1</li>
-            <button>Done</button>
-            <button>Delete</button>
-          </div>
-          <div className='list-row' >
-            <li>todo 2</li>
-            <button>Done</button>
-            <button>Delete</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div className='list-row' >
+                <li>{todo}</li>
+                <button>Done</button>
+                <button>Delete</button>
+              </div>
+            )
+          })}
         </ul>
       </div>
 
